8ca13ef5f64564b5f11793f860726894ebe5c11c
Aluana <Muslimova>
2026-09-20 18:56:15 +0500

    1 commit(structure and first interfaces

---
+++ lol/src/transport/Truck.java
@@ -0,0 +1,10 @@
+package transport;
+
+public class Truck implements Transport
+{
+    @Override
+    public void deliver(String cargo, String destination)
+    {
+        System.out.println("Truck delivers " + cargo + " to " + destination + " by road.");
+    }
+}
